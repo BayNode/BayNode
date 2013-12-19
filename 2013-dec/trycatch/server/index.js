5185bf2437d5e68a9e3dc16f04b526c5d00b0167
Adam Crabtree <dude@noderiety.com>
2013-12-18 19:55:53 -0800

    More try catch talk

--- 2013-dec/trycatch/server/index.js
+++ 2013-dec/trycatch/server/index.js
@@ -1,8 +1,19 @@
 var trycatch = require('trycatch');
 var express = require('express');
 
+trycatch.configure({'long-stack-traces': true});
 
 var app = express();
+app.use(function(req, res, next) {
+	trycatch(next, function(err) {
+		if (err.message === 'Fail.') {
+			throw new Error('not!');
+		}
+		res.writeHead(500);
+		console.log(err.stack);
+		res.end('Something wrong happened');
+	})
+})
 
 app.get('/hello', function(req, res){
 	var body = 'Hello World';
@@ -11,7 +22,23 @@ app.get('/hello', function(req, res){
 	res.end(body);
 });
 
+app.get('/error', function(req, res){
+	process.nextTick(function() {
+		throw new Error('Fail.');
+	})
+});
 
 app.listen(3000);
 console.log('Listening on port 3000');
+
+process.on('uncaughtException', function(err) {
+	console.log('Stop listening. Closing in 5s...', err.stack);
+	setTimeout(function() {
+		process.exit();
+	}, 5000);
+});
+
+process.on('uncaughtApplicationException', function(err) {
+	console.log('That\'s weird, but we\'re cool...');
+});
 
