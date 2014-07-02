50ad0687238f88793824bb72582e559d1ea8d80a
Adam Crabtree <dude@noderiety.com>
2014-07-01 20:47:57 -0700

    Update examples

--- 2014-jul/traceur/index.js
+++ 2014-jul/traceur/index.js
@@ -1,20 +1,29 @@
 // /*jshint esnext: true*/
 
+ 
 // Block scoping
-for (let i=1; i<10; i++) {
-	for (let i=1; i<10; i++) {
-		console.log('Block scoping:', i)
-	}
-}
+// for (let i=1; i<10; i++) {
+// 	for (let i=1; i<10; i++) {
+// 		console.log('Block scoping:', i)
+// 	}
+// }
 
 // Destructuring Assignment
-// let [a,b] = [1,2,3]
+// let [a,,b] = [1,2,3]
 // let {c, d} = {c: 4, d: 5}
 // console.log('Destructuring Assignment:', a, b, c, d)
 
-// Array Compprehensions
-// let arrComp = [for (x of 'abcdefgh'.split('')) for (y of '12345678'.split('')) (x+y)]
+// Array Comprehensions
+// let arrComp = [
+// 	for (x of 'abcdefgh'.split(''))
+// 		for (y of '12345678'.split(''))
+// 			(x+y)
+// ]
 // console.log('Array Comprehensions:', arrComp)
+
+// let arr = [{b: 0}, {b: 1}, {b: 2}, {b: 3}]
+// let bs = [for (x of arr) x.b]
+// console.log('Array Comprehensions:', bs)
 
 // Computed Property Names
 // let x = 'something really cool'
@@ -24,6 +33,7 @@ for (let i=1; i<10; i++) {
 // console.log('Computed Property Names:', obj[x])
 
 // Default Parameters
+// function bar(arg=true, arg2=!arg) {}
 // function foo(list, listLength = list.length, loggingKey = 'foo for List of length '+listLength) {
 // 	console.log('Default Parameters:', loggingKey)
 // }
@@ -37,7 +47,7 @@ for (let i=1; i<10; i++) {
 // 		return 'Tiki tiki tiki'
 // 	}
 // }
-// console.log('Property Method Assignment:', obj)
+// console.log('Property Method Assignment:', String(obj))
 
 // Object Initializer Shorthand
 // let a = 'hello', b = 'world'
@@ -48,61 +58,36 @@ for (let i=1; i<10; i++) {
 // function rest(someValue, ...options) {
 // 	console.log('Rest:', someValue, options)
 // }
-// foo('Tiki tiki tiki', true, false, 'hello')
+// rest('Tiki tiki tiki', true, false, 'hello')
 
 // Spread
 // let options = [true, false, 'hello']
 // rest('Tiki tiki tiki', ...options)
 
 // Template Literals
-// let firstName = 'Adam', lastName = 'Crabtree'
-// console.log('Template Literals:', `hello ${firstName} ${lastName}`)
+// let name = {first:'Adam',last:'Crabtree'}
+// console.log('Template Literals:', `hello ${name.first} ${name.last}`)
 
 // Arrow Functions
-// let arrowFn = function (x) {
+// let square = function (x) {
 // 	return x * x
 // }
-// let arrowFn = (x) => {
+// let square = (x) => {
 // 	return x * x
 // }
-// let arrowFn = (x) => x * x
-// let arrowFn = x => x * x
-// console.log('Arrow Functions:', arrowFn(2))
+// let square = (x) => x * x
+// let square = x => x * x
+// console.log('Arrow Functions:', square(2))
 
-// var arr = [1,2,3,4].map(value => value*2);
+// let arr = [{b: 0}, {b: 1}, {b: 2}, {b: 3}]
+// let bs = arr.map(value => value.b);
+// console.log('Arrow Functions:',bs)
 // process.nextTick(()=> console.log('hello'));
 
-// Advanced Combination
-var fn = ({a=true, b}) => {
-  // if (!options.a) options.a = true
-  console.log(a, b);
-}
+// Fat Arrow Inherited Context
+// function foo() {
+// 	return () => this
+// }
 
-fn({
-  // a: 'hello',
-  b: 'world'
-})
-// console.log(arr);
-
-
-
-// let that = this;
-// var obj = {
-//   a: a => b => c => {
-//     // let foo = 'asf';
-//     console.log(a,b,c)
-//   }
-// };
-
-// obj.a(1)(2)(3);
-// // (a => b => console.log(a, b, this === that))(1)(2);
-// // console.log(e => { console.log(e.name); });
-
-// export function a() {}
-
-
-// (function({a, b=3}) {
-//   console.log(a, b);
-// })({
-//   a: 1
-// })
+// let that = foo.apply({hello: 'world'})
+// console.log('Fat Arrow Inherited Context:', that)
